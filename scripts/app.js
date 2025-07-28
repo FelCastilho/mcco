9b94bdfa9a993ae360167a71fa87cc1fca70bcde
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-07-28 18:08:03 -0300

    finalizando header

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// SCROLL HEADER
 function scrollHeader(){
     const nav = document.getElementById("header");
 
@@ -6,3 +7,31 @@ function scrollHeader(){
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+//DROPDOWN MENU
+const dropdownMenu = document.getElementById("dropdown-item");
+const menuItem = document.getElementById("menu-item");
+
+menuItem.addEventListener("mouseenter", () => {
+    dropdownMenu.classList.add("active-dropdown-menu");
+});
+
+menuItem.addEventListener("mouseleave", () => {
+    dropdownMenu.classList.remove("active-dropdown-menu");
+});
+
+// MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener("click", () => {
+            nav.classList.toggle("active-menu-mobile");
+            toggle.classList.toggle("active-bx");
+        });
+    }
+}
+
+showMenu("bx", "menu-mobile");
